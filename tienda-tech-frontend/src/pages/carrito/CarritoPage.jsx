51a621aa60300fec23982ce95aec57c19e0bdccc
KevinTalledo <kevintalledo5b@gmail.com>
2026-09-05 16:18:16 -0500

    Funcion para vaciar productos del carrito

--- tienda-tech-frontend/src/pages/carrito/CarritoPage.jsx
+++ tienda-tech-frontend/src/pages/carrito/CarritoPage.jsx
@@ -23,10 +23,15 @@ export default function CarritoPage() {
     if (nuevaCantidad < 1) return;
     setCarrito(cambiarCantidad(productoId, nuevaCantidad));
   };
-  
+
   const handleQuitar = (productoId) => {
     setCarrito(quitarDelCarrito(productoId));
   };
 
+  const handleVaciar = () => {
+    vaciarCarrito();
+    setCarrito([]);
+  };
 
+  
 }
